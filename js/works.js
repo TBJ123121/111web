ea42bc05d37d24e050fd97918f86d05b0e2ea4ab
yenmin <s9208414@gmail.com>
2021-06-12 18:02:54 +0800

    加入挾帶groupId值的連結

--- js/works.js
+++ js/works.js
@@ -1,5 +1,6 @@
 var page = 1;
 var w;
+var groupID;
 
 window.onload = function(){
     watchChangeSize();
@@ -22,145 +23,145 @@ function showSlides(page){
       switch(page){
         case 1:
           father.innerHTML = '<div class="mySlides fade" id="11" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/11.jpg" style="width:100%"/></a>'+
+          '<img src="./assets/teamProject/11.jpg" style="width:100%" onclick="send(11)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="1" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/1.jpg" style="width:100%"/></a>'+
+            '<img src="./assets/teamProject/1.jpg" style="width:100%" onclick="send(1)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="2" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/2.jpg" style="width:100%"/></a>'+
+            '<img src="./assets/teamProject/2.jpg" style="width:100%" onclick="send(2)"/>'+
           '</div>';
           break;
         case 2:
           father.innerHTML = '<div class="mySlides fade" id="1" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/1.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/1.jpg" style="width:100%" onclick="send(1)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="2" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/2.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/2.jpg" style="width:100%" onclick="send(2)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="3" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/3.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/3.jpg" style="width:100%" onclick="send(3)"/>'+
           '</div>';
           break;  
         case 3:
           father.innerHTML = '<div class="mySlides fade" id="2" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/2.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/2.jpg" style="width:100%" onclick="send(2)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="3" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/3.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/3.jpg" style="width:100%" onclick="send(3)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="4" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/4.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/4.jpg" style="width:100%" onclick="send(4)"/>'+
           '</div>';
           break;
         case 4:
           father.innerHTML = '<div class="mySlides fade" id="3" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/3.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/3.jpg" style="width:100%" onclick="send(3)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="4" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/4.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/4.jpg" style="width:100%" onclick="send(4)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="5" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/5.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/5.jpg" style="width:100%" onclick="send(5)"/>'+
           '</div>';
           break;
         case 5:
           father.innerHTML = '<div class="mySlides fade" id="4" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/4.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/4.jpg" style="width:100%" onclick="send(4)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="5" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/5.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/5.jpg" style="width:100%" onclick="send(5)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="6" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/6.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/6.jpg" style="width:100%" onclick="send(6)"/>'+
           '</div>';  
           break;
         case 6:
           father.innerHTML = '<div class="mySlides fade" id="5" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/5.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/5.jpg" style="width:100%" onclick="send(5)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="6" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/6.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/6.jpg" style="width:100%" onclick="send(6)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="7" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/7.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/7.jpg" style="width:100%" onclick="send(7)"/>'+
           '</div>';
           break;
         case 7:
           father.innerHTML = '<div class="mySlides fade" id="6" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/6.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/6.jpg" style="width:100%" onclick="send(6)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="7" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/7.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/7.jpg" style="width:100%" onclick="send(7)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="8" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/8.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/8.jpg" style="width:100%" onclick="send(8)"/>'+
           '</div>';
           break;
         case 8:
           father.innerHTML = '<div class="mySlides fade" id="7" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/7.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/7.jpg" style="width:100%" onclick="send(7)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="8" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/8.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/8.jpg" style="width:100%" onclick="send(8)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="9" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/9.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/9.jpg" style="width:100%" onclick="send(9)"/>'+
           '</div>';
           break;    
         case 9:
           father.innerHTML = '<div class="mySlides fade" id="8" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/8.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/8.jpg" style="width:100%" onclick="send(8)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="9" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/9.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/9.jpg" style="width:100%" onclick="send(9)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="10" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/10.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/10.jpg" style="width:100%" onclick="send(10)"/>'+
           '</div>';
           break;      
         case 10:
           father.innerHTML = '<div class="mySlides fade" id="9" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/9.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/9.jpg" style="width:100%" onclick="send(9)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="10" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/10.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/10.jpg" style="width:100%" onclick="send(10)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="11" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/11.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/11.jpg" style="width:100%" onclick="send(11)"/>'+
           '</div>';
           break;
         case 11:
           father.innerHTML = '<div class="mySlides fade" id="10" style="display:block;">'+
-          '<a href="#"><img src="./assets/teamProject/10.jpg" style="width:100%"></a>'+
+          '<img src="./assets/teamProject/10.jpg" style="width:100%" onclick="send(10)"/>'+
           '</div>'+
 
           '<div class="mySlides" id="11" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/11.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/11.jpg" style="width:100%" onclick="send(11)"/>'+
           '</div>'+
 
           '<div class="mySlides fade" id="1" style="display:block;">'+
-            '<a href="#"><img src="./assets/teamProject/1.jpg" style="width:100%"></a>'+
+            '<img src="./assets/teamProject/1.jpg" style="width:100%" onclick="send(1)"/>'+
           '</div>';
           break;  
       }
@@ -170,77 +171,77 @@ function showSlides(page){
           case 1:
             father.innerHTML = 
             '<div class="mySlides" id="1" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/1.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/1.jpg" style="width:100%" onclick="send(1)"/>'+
             '</div>';
    
             break;
           case 2:
             father.innerHTML = 
             '<div class="mySlides" id="2" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/2.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/2.jpg" style="width:100%" onclick="send(2)"/>'+
             '</div>';
 
             break;  
           case 3:
             father.innerHTML =
             '<div class="mySlides" id="3" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/3.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/3.jpg" style="width:100%" onclick="send(3)"/>'+
             '</div>';
 
             break;
           case 4:
             father.innerHTML = 
             '<div class="mySlides" id="4" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/4.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/4.jpg" style="width:100%" onclick="send(4)"/>'+
             '</div>'
 
             break;
           case 5:
             father.innerHTML = 
             '<div class="mySlides" id="5" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/5.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/5.jpg" style="width:100%" onclick="send(5)"/>'+
             '</div>';  
 
             break;
           case 6:
             father.innerHTML = 
             '<div class="mySlides" id="6" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/6.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/6.jpg" style="width:100%" onclick="send(6)"/>'+
             '</div>';
 
             break;
           case 7:
             father.innerHTML = 
             '<div class="mySlides" id="7" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/7.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/7.jpg" style="width:100%" onclick="send(7)"/>'+
             '</div>';
 
             break;
           case 8:
             father.innerHTML = 
             '<div class="mySlides" id="8" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/8.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/8.jpg" style="width:100%" onclick="send(8)"/>'+
             '</div>';
 
             break;    
           case 9:
             father.innerHTML =
             '<div class="mySlides" id="9" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/9.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/9.jpg" style="width:100%" onclick="send(9)"/>'+
             '</div>';
 
             break;      
           case 10:
             father.innerHTML =
             '<div class="mySlides" id="10" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/10.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/10.jpg" style="width:100%" onclick="send(10)"/>'+
             '</div>';
 
             break;
           case 11:
             father.innerHTML = 
             '<div class="mySlides" id="11" style="display:block;">'+
-              '<a href="#"><img src="./assets/teamProject/11.jpg" style="width:100%"></a>'+
+              '<img src="./assets/teamProject/11.jpg" style="width:100%" onclick="send(11)"/>'+
             '</div>';
 
             break;  
@@ -263,4 +264,8 @@ function showReduceSlides() {
     page = 11;
   }
   showSlides(page);
+}
+
+function send(groupID){
+  location.href="works_member.html?id=" +groupID;
 }
