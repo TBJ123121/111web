0966e14ebba7768342c63d67e72b90fe607be138
cwyADT107102 <68813383+cwy104seung28@users.noreply.github.com>
2021-09-25 15:33:03 +0800

    Update works_member.php

--- works_member.php
+++ works_member.php
@@ -39,7 +39,7 @@
         </nav>
         <!-- Page Header-->
         <header class="masthead" id="anima">
-            <div class="overlay"></div>
+            <div class="overlay dct-overlay"></div>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-md-10 mx-auto">
